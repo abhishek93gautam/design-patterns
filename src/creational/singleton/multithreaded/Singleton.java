81be251674edc986ca46ad32e7a03f0c15c6d412
Abhishek <abugautam@gmail.com>
2023-09-06 17:55:06 +0530

    Initial commit,  added singleton creational desgin pattern

---
+++ src/creational/singleton/multithreaded/Singleton.java
@@ -0,0 +1,29 @@
+package creational.singleton.multithreaded;
+
+/*
+The same class behaves incorrectly in a multithreaded environment.
+Multiple threads can call the creation method simultaneously and get several instances of Singleton class.
+ */
+
+
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        // The following code emulates slow initialization.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
