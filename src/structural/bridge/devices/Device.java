a43e918834618c7500246dffe54347a7a58dae26
Abhishek <abugautam@gmail.com>
2023-10-04 08:51:41 +0530

    Added Bridge structural design pattern with examples and Demo

---
+++ src/structural/bridge/devices/Device.java
@@ -0,0 +1,22 @@
+package structural.bridge.devices;
+
+// Common interface of all devices
+
+public interface Device {
+
+    boolean isEnabled();
+
+    void enable();
+
+    void disable();
+
+    int getVolume();
+
+    void setVolume(int percent);
+
+    int getChannel();
+
+    void setChannel(int channel);
+
+    void printStatus();
+}
---
+++ src/structural/bridge/remotes/Remote.java
@@ -0,0 +1,16 @@
+package structural.bridge.remotes;
+
+// Common interface for all Remotes
+
+public interface Remote {
+
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
